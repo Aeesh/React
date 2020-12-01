fa2b07d6d58ab80213ee31a48418ecda346fae0a
Aeesh <adamaisha352@gmail.com>
2020-12-01 22:32:19 +0100

    react-router addd

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Contacts from './components/contacts/Contacts.js';
-import Header from './components/layout/Header';
 import AddContact from './components/contacts/AddContact';
+
+import Header from './components/layout/Header';
+import About from './components/pages/About';
 
 import { Provider } from './context';
 
@@ -12,13 +15,18 @@ class App extends Component {
   render() {
     return (
       <Provider>
-        <div >
-        <Header fellows = "TEAM 5"/>
-        <div className="container">
-          <AddContact />
-          <Contacts />
-        </div>
-      </div>
+        <Router>
+          <div >
+            <Header fellows = "TEAM 5"/>
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/contact/add" component={AddContact} />
+              </Switch>
+            </div>
+          </div>
+        </Router>
       </Provider>
     );
   }
